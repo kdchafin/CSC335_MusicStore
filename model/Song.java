4d2d97e360d3e8489dcfdcac28aa8e9aa43f3fc6
andyjsiegel <118780453+andyjsiegel@users.noreply.github.com>
2025-02-14 11:52:21 -0700

    Song and Album classes

---
+++ model/Song.java
@@ -0,0 +1,10 @@
+public class Song {
+
+    private String title;
+    private String author;
+
+    public Song(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
