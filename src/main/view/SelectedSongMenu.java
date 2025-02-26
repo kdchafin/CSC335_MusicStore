764bac5707c7ef90db80eac24d663edaaa87abba
andyjsiegel <andyjsiegel1@gmail.com>
2025-02-25 23:22:05 -0700

    Add Song To Playlist and using library instance variable to hold the global instance rather than calling getInstance in each option method.

--- src/main/view/SelectedSongMenu.java
+++ src/main/view/SelectedSongMenu.java
@@ -3,8 +3,11 @@ package main.view;
 import main.model.Song;
 import main.model.LibraryModel;
 
+import java.util.Scanner;
+
 public class SelectedSongMenu extends Menu {
-    Song song;
+    private LibraryModel library;
+    private Song song;
     public SelectedSongMenu(Menu previousMenu, Song song) {
         super("""
         [1] Add Song To Library
@@ -13,21 +16,28 @@ public class SelectedSongMenu extends Menu {
         [4] Mark Song As Favorite
         """.trim());
         this.song = song;
+        this.library = LibraryModel.getInstance();
         defaultOption = () -> { previousMenu.executeMenu(); };
         addOption(1, "add song to library", () -> { addSongToLibrary(); executeMenu(); });
-        addOption(2, "add song to playlist", () -> { System.out.println("Add Song To Playlist // Not Implemented Yet"); executeMenu(); });
+        addOption(2, "add song to playlist", () -> { addSongToPlaylist(); executeMenu(); });
         addOption(3, "rate song", () -> { rateSong(); });
-        addOption(4, "mark song as favorite", () -> { markSongAsFavorite(); });
+        addOption(4, "mark song as favorite", () -> { markSongAsFavorite(); executeMenu(); });
     }
 
     private void addSongToLibrary() {
-        LibraryModel library = LibraryModel.getInstance();
         library.addSong(this.song);
         System.out.println("Song Added To Library");
     }
 
+    private void addSongToPlaylist() {
+        Scanner in = Menu.getScanner();
+        System.out.println("Enter a playlist name: ");
+        String name = in.nextLine();
+        String result = library.addSongToPlaylist(this.song, name);
+        System.out.println(result);
+    }
+
     private void markSongAsFavorite() {
-        LibraryModel library = LibraryModel.getInstance();
         this.song.setRating(5);
         library.addSong(this.song);
         System.out.println(this.song.getTitle() + " Marked As Favorite!");
