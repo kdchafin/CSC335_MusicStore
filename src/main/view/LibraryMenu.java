1e70062b6ea6a233172754559a3a90e7819dd9de
andyjsiegel <andyjsiegel1@gmail.com>
2025-02-25 23:18:51 -0700

    functionality for all 8 options besides the 2 TODOs, getSongs, and getFavoriteSongs. getSongs and getFavoriteSongs now are not ugly

--- src/main/view/LibraryMenu.java
+++ src/main/view/LibraryMenu.java
@@ -1,8 +1,11 @@
 package main.view;
 
-import main.model.LibraryModel;
+import main.model.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LibraryMenu extends Menu {
+    private LibraryModel library;
     public LibraryMenu(Menu previousMenu) {
         super("""
         [1] Get Songs
@@ -10,20 +13,90 @@ public class LibraryMenu extends Menu {
         [3] Get Artists
         [4] Get Playlists
         [5] Get Favorite Songs
+        [6] Create Playlist
+        [7] Remove A Song From A Playlist
+        [8] Delete Playlist
         """.trim());
-        LibraryModel library = LibraryModel.getInstance();
+        library = LibraryModel.getInstance();
         defaultOption = () -> { previousMenu.executeMenu(); };
-        addOption(1, "get songs", () -> { 
-            // System.out.println("[1] Get Songs Chosen"); 
-            System.out.println(library.getSongs()); 
-            executeMenu();
-        });
-        addOption(2, "get albums", () -> { System.out.println("Get Albums"); executeMenu();});
-        addOption(3, "get artists", () -> { System.out.println("Get Artists"); executeMenu();});
-        addOption(4, "get playlists", () -> { System.out.println("Get Playlists"); executeMenu();});
-        addOption(5, "get favorite songs", () -> { 
-            System.out.println(library.getFavoriteSongs()); 
-            executeMenu();
-        });
+
+        addOption(1, "get songs", () -> { getSongs(); executeMenu(); });
+        addOption(2, "get albums", () -> { System.out.println("Get Albums"); executeMenu(); }); //TODO: Add Get Albums Functionality For The Library
+        addOption(3, "get artists", () -> { System.out.println("Get Artists"); executeMenu(); }); //TODO: Add Get Artists Functionality For The Library
+        addOption(4, "get playlists", () -> { printEachElement(library.getPlaylists(), "playlist"); executeMenu(); });
+        addOption(5, "get favorite songs", () -> { getFavoriteSongs(); executeMenu(); });
+        addOption(6, "create a playlist", () -> { createPlaylist(); executeMenu(); });
+        addOption(7, "remove a song from a playlist", () -> { editPlaylist(); executeMenu(); });
+        addOption(8, "delete a playlist", () -> { deletePlaylist(); executeMenu(); });
+    }
+
+    private void getSongs() {
+        String plural = library.getSongs().size() == 1 ? "" : "s";
+        System.out.println("You have " + library.getSongs().size() + " song" + plural + " in your library:");
+        printEachElement(library.getSongs(), "song");
+    }
+
+    private void getFavoriteSongs() {
+        ArrayList<Song> favoriteSongs = library.getFavoriteSongs();
+        String plural = favoriteSongs.size() == 1 ? "" : "s";
+        if(favoriteSongs.size() == 0) {
+            System.out.println("You have no favorite songs");
+        } else {
+            System.out.println("You have " + favoriteSongs.size() + " favorite song" + plural + ":");
+            printEachElement(library.getFavoriteSongs(), "favorite song"); 
+        } 
+    }
+
+    private void createPlaylist() {
+        Scanner in = Menu.getScanner();
+        System.out.println("Enter a playlist name: ");
+        String name = in.nextLine();
+        String result = library.createPlaylist(name);
+        System.out.println(result);
+    }
+
+    private void editPlaylist() {
+        Scanner in = Menu.getScanner();
+        System.out.println("Enter a playlist name: ");
+        String name = in.nextLine();
+        ArrayList<Playlist> playlists = library.getPlaylists();
+        Playlist playlist = null;
+        boolean foundPlaylist = false;
+        for(Playlist pl : playlists) {
+            if(pl.getName().equals(name)) {
+                foundPlaylist = true;
+                playlist = pl;
+                System.out.println("- " + pl);
+                break;
+            }
+        }
+        if(!foundPlaylist) {
+            System.out.println("There is no playlist with that name.");
+            return;
+        }
+        System.out.println("What number song would you like to remove from the playlist?");
+        int index = Integer.parseInt(in.nextLine());
+        try { 
+            Song song = playlist.removeSong(index-1); 
+            System.out.println(song.getTitle() + " removed from " + playlist.getName());
+        } catch (IndexOutOfBoundsException e) { 
+            System.out.println("There is no song at that index"); 
+        }
+    }
+
+    private void deletePlaylist() {
+        Scanner in = Menu.getScanner();
+        System.out.println("Enter a playlist name: ");
+        String name = in.nextLine();
+        library.deletePlaylist(name);
+    }
+
+    private <T> void printEachElement(ArrayList<T> elements, String type) {
+        if(elements.size() == 0) {
+            System.out.println("There are no " + type + "s in the library.");
+        }
+        for(T element : elements) {
+            System.out.println("- " + element);
+        }
     }
 }
