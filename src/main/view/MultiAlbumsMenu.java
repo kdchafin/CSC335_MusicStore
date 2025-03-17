13ef8fbe0693b07c12162bad1c3133146d67fa2f
andyjsiegel <andyjsiegel1@gmail.com>
2025-03-17 13:37:57 -0700

    add inBoolean flag

--- src/main/view/MultiAlbumsMenu.java
+++ src/main/view/MultiAlbumsMenu.java
@@ -12,7 +12,8 @@ public class MultiAlbumsMenu extends Menu {
         sb.append(album.size() + " Albums Found:");
         for(Album a : album) {
             index++;
-            addOption(index, a.getTitle(), () -> { System.out.println("You selected " + a.getTitle()); SelectedAlbumMenu selectedAlbumMenu = new SelectedAlbumMenu(this, a); selectedAlbumMenu.executeMenu(); });
+            boolean inLibrary = previousMenu.getClass().getSimpleName().equals("LibraryMenu");
+            addOption(index, a.getTitle(), () -> { System.out.println("You selected " + a.getTitle()); SelectedAlbumMenu selectedAlbumMenu = new SelectedAlbumMenu(this, a, inLibrary); selectedAlbumMenu.executeMenu(); });
             sb.append("\n[" + index + "] " + a.getTitle());
         }
         menu += sb.toString();
