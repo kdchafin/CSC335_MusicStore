b285f677274de36ad86444dc3e7a51a37040aa9c
andyjsiegel <andyjsiegel1@gmail.com>
2025-03-17 13:38:40 -0700

    add inLibrary flag

--- src/main/view/SelectedAlbumMenu.java
+++ src/main/view/SelectedAlbumMenu.java
@@ -6,19 +6,29 @@ import main.model.LibraryModel;
 public class SelectedAlbumMenu extends Menu {
     private LibraryModel library;
     private Album album;
-    public SelectedAlbumMenu(Menu previousMenu, Album album) {
+    public SelectedAlbumMenu(Menu previousMenu, Album album, boolean inLibrary) {
         super("""
-        [1] Add Album to Library
         """.trim());
+        if(inLibrary) {
+            addOption(1, "remove album from library", () -> { removeAlbumFromLibrary(); previousMenu.executeMenu(); });
+            menu = "[1] Remove Album From Library" + menu;
+        } else {
+            addOption(1, "add album to library", () -> { addAlbumToLibrary(); previousMenu.executeMenu(); });
+            menu = "[1] Add Album To Library\n" + menu;
+        }
         colorizeBrackets();
         this.album = album;
         this.library = LibraryModel.getInstance();
         defaultOption = () -> { previousMenu.executeMenu(); };
-        addOption(1, "add album to library", () -> { addAlbumToLibrary(); previousMenu.executeMenu(); });
     }
 
     private void addAlbumToLibrary() {
         library.addAlbum(album);
         System.out.println("Album Added To Library");
+    }
+
+    private void removeAlbumFromLibrary() {
+        // library.removeAlbum(album);
+        System.out.println("Album Removed From Library");
     }
 }
--- src/main/view/SelectedSongMenu.java
+++ src/main/view/SelectedSongMenu.java
@@ -8,19 +8,25 @@ import java.util.Scanner;
 public class SelectedSongMenu extends Menu {
     private LibraryModel library;
     private Song song;
-    public SelectedSongMenu(Menu previousMenu, Song song) {
+    public SelectedSongMenu(Menu previousMenu, Song song, boolean inLibrary) {
         super("""
-        [1] Add Song To Library
         [2] Add Song To Playlist
         [3] Rate Song
         [4] Mark Song As Favorite
         [5] Play Song
         """.trim());
+        if(inLibrary) {
+            addOption(1, "remove song from library", () -> { removeSongFromLibrary(); executeMenu(); });
+            menu = "[1] Remove Song From Library\n" + menu;
+        } else {
+            addOption(1, "add song to library", () -> { addSongToLibrary(); executeMenu(); });
+            menu = "[1] Add Song To Library\n" + menu;
+        }
         colorizeBrackets();
         this.song = song;
         this.library = LibraryModel.getInstance();
         defaultOption = () -> { previousMenu.executeMenu(); };
-        addOption(1, "add song to library", () -> { addSongToLibrary(); executeMenu(); });
+        
         addOption(2, "add song to playlist", () -> { addSongToPlaylist(); executeMenu(); });
         addOption(3, "rate song", () -> { rateSong(); });
         addOption(4, "mark song as favorite", () -> { markSongAsFavorite(); executeMenu(); });
@@ -30,6 +36,10 @@ public class SelectedSongMenu extends Menu {
     private void addSongToLibrary() {
         library.addSong(this.song);
         System.out.println("Song Added To Library");
+    }
+
+    private void removeSongFromLibrary() {
+        library.removeSong(this.song);
     }
 
     private void addSongToPlaylist() {
