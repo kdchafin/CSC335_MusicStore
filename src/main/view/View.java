d0fbfea048acbe31f89e05ded3a1c071baf8956c
andyjsiegel <andyjsiegel1@gmail.com>
2025-03-22 16:50:31 -0700

    Removed useless comment

--- src/main/view/View.java
+++ src/main/view/View.java
@@ -5,12 +5,6 @@ import java.io.FileNotFoundException;
 import main.model.*;
 
 public class View {
-    /* Users need to be able to do the following: 
-     * Search for songs by title or artist
-     * Search for albums by title or artist
-     * When returning a song from search, print title, artist, and album.
-     * Search from user library. 
-     */
     public static void main(String[] args) throws FileNotFoundException {
         MusicStore musicStore = MusicStore.getInstance();
         musicStore.generateDataset();
