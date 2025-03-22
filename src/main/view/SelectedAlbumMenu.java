cc926fbc2ae4732df7f945130065961893ab81d9
andyjsiegel <andyjsiegel1@gmail.com>
2025-03-22 16:50:07 -0700

    addAlbumToPlaylist method

--- src/main/view/SelectedAlbumMenu.java
+++ src/main/view/SelectedAlbumMenu.java
@@ -1,17 +1,23 @@
 package main.view;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import main.model.Album;
+import main.model.Song;
 import main.model.LibraryModel;
+import main.model.Playlist;
 
 public class SelectedAlbumMenu extends Menu {
     private LibraryModel library;
     private Album album;
     public SelectedAlbumMenu(Menu previousMenu, Album album, boolean inLibrary) {
-        super("""
-        """.trim());
+        super("");
         if(inLibrary) {
             addOption(1, "remove album from library", () -> { removeAlbumFromLibrary(); previousMenu.executeMenu(); });
-            menu = "[1] Remove Album From Library" + menu;
+            addOption(2, "add album to playlist", () -> { addAlbumToPlaylist(); previousMenu.executeMenu(); });
+            menu = "[1] Remove Album From Library\n" + menu;
+            menu += "[2] Add Album To Playlist";
         } else {
             addOption(1, "add album to library", () -> { addAlbumToLibrary(); previousMenu.executeMenu(); });
             menu = "[1] Add Album To Library\n" + menu;
@@ -28,7 +34,22 @@ public class SelectedAlbumMenu extends Menu {
     }
 
     private void removeAlbumFromLibrary() {
-        // library.removeAlbum(album);
+        library.removeAlbum(album);
         System.out.println("Album Removed From Library");
+    }
+
+    private void addAlbumToPlaylist() {
+        Scanner in = Menu.getScanner();
+        System.out.println("Enter a playlist name: ");
+        String name = in.nextLine();
+        library.addAlbum(album);
+        for(Song s: album.getAlbumSongs()) {
+            String temp = library.addSongToPlaylist(s, name);
+            if(!temp.equals(s.getTitle() + " added to " + name + " successfully!")) {
+                System.out.println(temp);
+                return;
+            }
+        }
+        System.out.println(album.getTitle() + " added to " + name + " successfully!");
     }
 }
